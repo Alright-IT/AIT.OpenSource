e84ab71062cd902fbd64e2ef0c8b64428f62e99d
Joerg Hochwald <j.hochwald@alright-it.com>
2016-10-17 20:38:19 +0200

    Removed stuff no longer needed within PowerShell 5, or newer

---
+++ Public/Get-Clipboard.ps1
@@ -0,0 +1,53 @@
+﻿#!/usr/bin/env powershell
+#requires -Version 2.0
+
+if (($PSVersionTable.PSVersion.Major) -lt '5') {
+	<#
+			PowerShell older then Version 5 is found!
+	#>
+
+	function Get-Clipboard {
+		<#
+				.SYNOPSIS
+				Get the actual content of the Clipboard
+
+				.DESCRIPTION
+				Get the actual content of the Clipboard
+
+				.NOTES
+				It works in PowerShell STA Mode only!
+
+				.EXAMPLE
+				PS C:\> $foo = (Get-Clipboard)
+
+				Description
+				-----------
+				Get the content of the Clipboard and set it to the variable 'foo'
+
+				.LINK
+				Set-Clipboard
+		#>
+
+		[CmdletBinding()]
+		param ()
+
+		PROCESS {
+			if ($Host.Runspace.ApartmentState -eq 'STA') {
+				Add-Type -AssemblyName PresentationCore
+				[Windows.Clipboard]::GetText()
+			} else {
+				Write-Warning -Message ('Run {0} with the -STA parameter to use this function' -f $Host.Name)
+			}
+		}
+	}
+} else {
+	<#
+			PowerShell 5, or newer is found!
+
+			We do not need that function anymore ;-)
+
+			Use the Build-In one instead...
+	#>
+
+	Write-Verbose -Message 'This function is only needed if PowerShell is older then 5!'
+}
---
+++ Public/Set-Clipboard.ps1
@@ -0,0 +1,63 @@
+﻿#!/usr/bin/env powershell
+#requires -Version 3.0
+
+if (($PSVersionTable.PSVersion.Major) -lt '5') {
+	<#
+			PowerShell older then Version 5 is found!
+	#>
+
+	function Set-Clipboard {
+		<#
+				.SYNOPSIS
+				Copy content to the Clipboard
+
+				.DESCRIPTION
+				Copy content to the Clipboard
+
+				.PARAMETER Import
+				Content to import to the Clipboard
+
+				.EXAMPLE
+				PS C:\> Set-Clipboard -Import $foo
+
+				Description
+				-----------
+				It imports the content of the variable $foo to the Clipboard.
+
+				.NOTES
+				STA Mode only!
+
+				.LINK
+				Get-Clipboard
+		#>
+
+		param
+		(
+			[Parameter(Mandatory,
+					ValueFromPipeline,
+					Position = 0,
+			HelpMessage = 'Content to import')]
+			[ValidateNotNullOrEmpty()]
+			[String]$Import
+		)
+
+		PROCESS {
+			if ($Host.Runspace.ApartmentState -eq 'STA') {
+				Add-Type -AssemblyName PresentationCore
+				[Windows.Clipboard]::SetText($Import)
+			} else {
+				Write-Warning -Message ('Run {0} with the -STA parameter to use this function' -f $Host.Name)
+			}
+		}
+	}
+} else {
+	<#
+			PowerShell 5, or newer is found!
+
+			We do not need that function anymore ;-)
+
+			Use the Build-In one instead...
+	#>
+
+	Write-Verbose -Message 'This function is only needed if PowerShell is older then 5!'
+}
